c36489a0db318fc1944c294953a2f589371e36a9
Alexander Diaz <aliensanderdiaz@gmail.com>
2023-07-09 23:34:20 -0500

    fin

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,16 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import Mapboxgl from 'mapbox-gl'; // or "const Mapboxgl = require('mapbox-gl');"
+import { environment } from './environments/environment';
+
+Mapboxgl.accessToken = environment.apiKey
+
+if (!navigator.geolocation) {
+  alert('Adios no puedo trbaajr aqui')
+  throw new Error('Adios no puedo trbaajr aqui')
+}
+
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
